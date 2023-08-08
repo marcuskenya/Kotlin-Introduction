8981b879e0a54b4494fa740b7281366213c8bec0
unknown <marcusmbugua1@gmail.com>
2023-08-08 09:55:18 +0300

    Kotlin Introduction

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    //variables in kotlin
+    var name ="Maks"
+    var age = 18
+    println(name)
+    println(age)
+
+    var city:String= "Nairobi"
+    var idno:Int=1234654
+
+
+
+
+}
---
+++ src/main/kotlin/varfile.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    var num1 =2
+    var num2=98
+
+    var answer = num2 / num1
+    print(answer)
+
+
+    var num3 =29
+    var num4=98
+
+    val result = num3 + num4
+    print(result)
+
+//
+    var num5 =24
+    var num6=98
+
+    val value = num5 * num6
+    print(value)
+
+
+}
