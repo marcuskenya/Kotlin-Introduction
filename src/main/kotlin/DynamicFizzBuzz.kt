13a2d7b49152fc4ae25bead49e10150126d8646d
unknown <marcusmbugua1@gmail.com>
2023-08-11 10:05:39 +0300

    Working on loops

---
+++ src/main/kotlin/DynamicFizzBuzz.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+
+    //for the ranges 1..100
+    //User to enter 1st and 2nd number on the terminal
+    println("Enter 1st number")
+    var num1 = readln()!!.toInt()
+    println("Enter 2nd number")
+    var num2 = readln()!!.toInt()
+
+    for (j in num1..num2) {
+
+
+     if (j % 3 == 0  &&  j % 5 == 0)  {
+          println("FizzBuzz")
+     }
+       else if (j % 3 == 0 ) {
+         println("Fizz")
+
+       }else if(j % 5 == 0 ) {
+          println("Buzz")
+       }else
+           println(j)
+
+    }
+
+
+
+
+
+    }
